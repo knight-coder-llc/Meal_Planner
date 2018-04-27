c3dc4effe9bbdba96b1fc765a258e399b8d94773
reefman001 <brian.kilburn2@yahoo.com>
2018-04-26 22:45:26 -0400

    Add files via upload

---
+++ Ingredient.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * author: D. Koger
+ */
+public class Ingredient {
+    private int id;
+    private String name;
+    private int calories;
+    private int fat;
+    private int sodium;
+    private String group;
+    private int protein;
+    private int sugar;
+    //full constructor
+    Ingredient(int id, String n, int cal, int f, int so, String g, int p, int s){
+        this.id = id;
+        name = n;
+        calories = cal;
+        fat = f;
+        sodium = so;
+        group = g;
+        protein = p;
+        sugar = s;
+    }
+    //instructor without id
+    Ingredient(String n, int cal, int f, int so, String g, int p, int s){
+        name = n;
+        calories = cal;
+        fat = f;
+        sodium = so;
+        group = g;
+        protein = p;
+        sugar = s;
+    }
+    //null constructor
+    Ingredient(){
+        
+    }
+    public int getID(){
+        return id;       
+    }
+    public String getName(){
+        return name;
+    }
+    public int getCalories(){
+        return calories;
+    }
+    public int getFat(){
+        return fat;
+    }
+    public int getSodium(){
+        return sodium;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public int getProtien(){
+        return protein;
+    }
+    public int getSugar(){
+        return sugar;
+    }
+    public void setID(int id){
+        this.id = id;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setCalories(int c){
+        calories = c;
+    }
+    public void setFat(int f){
+        fat = f;
+    }
+    public void setSodium(int s){
+        sodium = s;
+    }
+    public void setGroup(String g){
+        group = g;
+    }
+    public void setProtein(int p){
+        protein = p;
+    }
+    public void setSugar(int s){
+        sugar = s;
+    }
+}
---
+++ Recipe.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * author: D Koger
+ */
+public class Recipe {
+    int recipeID;
+    String catagory;
+    String instruction;
+    String name;
+    //full Constructor
+    Recipe(int id, String cat, String instruct, String n){
+        recipeID = id;
+        catagory = cat;
+        instruction = instruct;
+        name = n;
+    }
+    //constructor without id
+    Recipe(String cat, String instruct, String n){
+        catagory = cat;
+        instruction = instruct;
+        name = n;
+    }
+    //null constructor
+    Recipe(){
+        
+    }
+    public int getID(){
+        return recipeID;
+    }
+    public String getCategory(){
+        return catagory;
+    }
+    public String getInstruction(){
+        return instruction;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setID(int id){
+        recipeID = id;
+    }
+    public void setCategory(String c){
+        catagory = c;
+    }
+    public void setInstruction(String i){
+        instruction = i;
+    }
+    public void setName(String n){
+        name = n;
+    }
+}
